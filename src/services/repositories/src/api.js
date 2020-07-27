cb069bd016adfc1a349d32455dca3ff6bae64829
Villalobos <diegovillalobos397@gmail.com>
2020-07-27 23:49:30 +0200

    Added repositories service api

---
+++ src/services/repositories/src/api.js
@@ -0,0 +1,42 @@
+import superagent from 'superagent';
+
+class Repositories {
+  constructor(GITHUB_API_URL) {
+    this.BASE_URL = GITHUB_API_URL;
+    this.SEARCH_REPOS_URL = '/search/repositories';
+    this.USER_AGENT = 'Shop-Apotheke-Coding-Challenge';
+  }
+
+  generateURL(inputURL, params) {
+    return Object
+      .entries(params)
+      .reduce((acc, [key, value]) => acc.concat(`${key}=${value}&`), `${this.BASE_URL}${inputURL}?`);
+  }
+
+  async getRepositories({
+    language, limit, sorting, createdAt,
+  }) {
+    const params = { q: '' };
+
+    if (language) params.q = params.q.concat(`language:${language}`);
+    if (createdAt) params.q = params.q.concat(`${language ? '+' : ''}created:>${createdAt}`);
+
+    if (sorting) {
+      const { field, direction } = sorting;
+
+      params.sort = field;
+      params.order = direction;
+    }
+
+    const url = this.generateURL(this.SEARCH_REPOS_URL, params);
+
+    const { body: result } = await superagent
+      .get(url)
+      .set('User-Agent', this.USER_AGENT)
+      .set('Accept', 'application/vnd.github.mercy-preview+json');
+
+    return { data: result.items.slice(0, limit) };
+  }
+}
+
+export default Repositories;
