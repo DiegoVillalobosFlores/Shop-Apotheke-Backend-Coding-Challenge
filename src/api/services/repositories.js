ed3d4ec77e62b0ea2eee270b1ae0321cb9719f13
Villalobos <diegovillalobos397@gmail.com>
2020-07-28 16:28:50 +0200

    Added more specific routes for most popular and top 10,50 and 100 repositories

--- src/api/services/repositories.js
+++ src/api/services/repositories.js
@@ -3,5 +3,25 @@ import Repositories from '../../services/repositories/src';
 const repositories = Repositories();
 
 export default [
-  ['get', '/getRepositories', (request) => repositories.getRepositories(request.query)],
+  ['get', '/getRepos', (request) => repositories.getRepositories(request.query)],
+  [
+    'get',
+    '/getMostPopularRepos',
+    (request) => repositories.getRepositories({ ...request.query, sort: 'stars' }),
+  ],
+  [
+    'get',
+    'getTop10Repos',
+    (request) => repositories.getRepositories({ ...request.query, sort: 'stars', limit: 10 }),
+  ],
+  [
+    'get',
+    'getTop50Repos',
+    (request) => repositories.getRepositories({ ...request.query, sort: 'stars', limit: 50 }),
+  ],
+  [
+    'get',
+    'getTop100Repos',
+    (request) => repositories.getRepositories({ ...request.query, sort: 'stars', limit: 100 }),
+  ],
 ];
